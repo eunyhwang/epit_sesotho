e0d018055a5347986efc8ef5547e88621ec72355
eunyhwang <49271495+eunyhwang@users.noreply.github.com>
2022-08-30 18:39:38 +0200

    Update setup.r

--- epitweetr/api/setup.r
+++ epitweetr/api/setup.r
@@ -1,8 +1,12 @@
 #install.packages("vscDebugger")
 #install.packages("plumber")
 #install.packages("ggplot")
+#install.packages("mapview")
+#install.packages("rgdal")
 library(plumber)
 library(ggplot2)
+library(mapview)
+library(rgdal)
 
 pr("endpoint.R") %>%
 pr_run(port=8000)
