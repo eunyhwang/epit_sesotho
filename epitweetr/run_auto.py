325c6891b923b6e1a3ccf5cb1cef8d0167c80937
eunyhwang <49271495+eunyhwang@users.noreply.github.com>
2022-09-26 18:34:57 +0200

    updated with environment variables

--- epitweetr/run_auto.py
+++ epitweetr/run_auto.py
@@ -2,19 +2,18 @@
 
 import subprocess
 import os
-from subprocess import Popen, PIPE
-import glob
+from subprocess import Popen
+from dotenv import load_dotenv
 
-''' make the path automaitcally
-path = os.system("path/run_epit")
-path = os.replace(a for a in file, location) 
-'''
+load_dotenv()
+path = Path(os.getenv("DATA_PATH"))
+
 file = ['main_loop', 'detect_loop', 'search_loop', 'fs_loop']
 #subprocess.run()
 #shell=True: string- open another terminal, takes more time / shell=False: list - keep one terminal 
-for a in file: # better way?
+for a in file: 
     print(a)
-    subprocess.Popen('my_path/run_epit/%s.R' % a, shell=True)
+    subprocess.Popen(f'Rscript {path}/run_epit/%s.R' % a, shell=True)
     #process = subprocess.Popen('Rscript my_path/run_epit/%s.R' % a, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) #stdout: save output into the variable(process)/ stderr: save error into process
     #rc = process.wait()
     #out, err = process.communicate()
