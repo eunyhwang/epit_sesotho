95942a29bab0092038d4991f171b0fa99f525fef
eunyhwang <49271495+eunyhwang@users.noreply.github.com>
2022-11-09 16:08:20 +0100

    Solved the 500 internal error by serializing

--- api_tosend/endpoint.r
+++ api_tosend/endpoint.r
@@ -1,75 +1,78 @@
-#library(dotenv)
-#load_dot_env()
-#data_path <- Sys.getenv("DATA_PATH")
-
-#* @post /epitweetr/top_words
-#* @serializer json list(na='string')
+#* @get /epitweetr/top_words
+#* @serializer png
 #* @param cntry country name
 #* @param num number of top words
-function(tpc="") { 
+#* @param tpc topic to query
+#* @param start indicating start of the reporting period
+#* @param end indicating end of the reporting period
+function(tpc="", cntry="", num="", start="", end="") { 
     epitweetr::setup_config(data_path)
     topwords <- epitweetr::create_topwords(
-        topic = tpc, 
-        country_codes = c("LS"),
-        date_min = "2022-08-01",
-        date_max = "2022-08-30",
+        topic = tpc,
+        country_codes = cntry,
+        date_min = start,
+        date_max = end,
         with_retweets = FALSE, 
         location_type = "tweet", 
-        top = 25
+        top = num
     )
-    print(topwords)
+    plot(topwords$chart)
 }
 
-#change the name into top_charts and put more than one param
-#* @post /epitweetr/top_entities 
+#* @get /epitweetr/top_entities
+#* @serializer png
 #* @param cntry country name
-#* @param tpc disease name
-function(tpc="") {
+#* @param num number of top words
+#* @param tpc topic to query
+#* @param start indicating start of the reporting period
+#* @param end indicating end of the reporting period
+function(tpc="", cntry="", num="", start="", end="") { 
     epitweetr::setup_config(data_path)
     topchart <- epitweetr::create_topchart(
-        topic= tpc,
+        topic = tpc,
         serie="entities",
-        country_codes = c("LS"),
-        date_min = "2022-08-01",
-        date_max = "2022-08-31",
-        with_retweets = FALSE,
-        location_type = "tweet",
-        top = 25
+        country_codes = cntry,
+        date_min = start,
+        date_max = end,
+        with_retweets = FALSE, 
+        location_type = "tweet", 
+        top = num
     )
-    print(topchart)
+    plot(topchart$chart)
 }
 
 #* @get /epitweetr/create_maps
-#* @param tpc disease name
-function(tpc="") {
+#* @serializer png
+#* @param cntry country name # names hould be in a full name (e.g. Lesotho)
+#* @param tpc topic to query
+#* @param start indicating start of the reporting period
+#* @param end indicating end of the reporting period
+function(tpc="", cntry="", start="", end="") { 
     epitweetr::setup_config(data_path)
     map <- epitweetr::create_map(
-        topic=tpc, 
-        countries = "African Region",
-        date_min = "2022-08-01",
-        date_max = "2022-08-31",
-        with_retweets = FALSE,
-        location_type = "tweet"
+        topic = tpc,
+        countries = cntry,
+        date_min = start,
+        date_max = end,
+        with_retweets = FALSE, 
+        location_type = "tweet", 
     )
-    print(map)
+    plot(map$chart)
 }
 
 #* @get /epitweetr/trend_line
-#* @param tpc disease name
-function(tpc="") {
+#* @serializer png
+#* @param cntry country name
+#* @param tpc topic to query
+#* @param start indicating start of the reporting period
+#* @param end indicating end of the reporting period
+function(tpc="", cntry="" , start="", end="") { 
     epitweetr::setup_config(data_path)
-    line <- epitweetr::trend_line(
-        topic =tpc,
-        countries = "African Region",
-        date_type = "created_date",
-        date_min = "2022-08-01",
-        date_max = "2022-08-30",
-        with_retweets = FALSE, 
-        location_type = "tweet",
-        alpha = 0.025,
-        alpha_outlier = 0.05,
-        k_decay= 4,
-        no_historic=7
+    trendline <- epitweetr::trend_line(
+        topic = tpc,
+        countries = cntry,
+        date_min = start,
+        date_max = end,
     )
-    print(line)
+    plot(trendline$chart)
 }
