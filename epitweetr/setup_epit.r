1e762d9d1bfb2a664e2ae11f45d6d070dce1eeac
eunyhwang <49271495+eunyhwang@users.noreply.github.com>
2023-01-13 16:39:27 +0100

    removed unnecessary line

--- epitweetr/setup_epit.r
+++ epitweetr/setup_epit.r
@@ -1,5 +1,4 @@
 setup_epit <- function(){ 
-    install.packages("epitweetr")
     # check if JAVA_HOME environment set up 
     result <- Sys.getenv("JAVA_HOME")
     if (is.null(result) == TRUE) {
