3b5c87326f109f25e6b10c1f03b56f8bd6e72aae
eunyhwang <49271495+eunyhwang@users.noreply.github.com>
2023-01-18 03:45:49 +0100

    Update bin_to_vec.py

--- fasttext/scripts/bin_to_vec.py
+++ fasttext/scripts/bin_to_vec.py
@@ -4,7 +4,7 @@ import argparse
 language_model = load_model("/put_the_path_of_your_model") 
 
 words = language_model.get_words()
-with open("/home/hwange/fastText/cbow_300.vec","w") as file_out: #open a VEC file to write down
+with open("/put_the_path_of_your_model","w") as file_out: #open a VEC file to write down
     #the first line contan number of total words and vector dimension
     file_out.write(str(len(words)) + " " + str(language_model.get_dimension()) + "\n")
     print("it's going well")
