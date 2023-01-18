d6024452e347d4bd76eb88c08b4eb0a0e2751a52
eunyhwang <49271495+eunyhwang@users.noreply.github.com>
2023-01-18 17:50:39 +0100

    Update train.py

--- fasttext/scripts/train.py
+++ fasttext/scripts/train.py
@@ -1,6 +1,26 @@
 import re
 import fasttext
-    
+
+from spacy.lang.af import Afrikaans
+
+nlp = Afrikaans()
+
+# Preprcessing
+
+print("preprocessing")
+with open('/path_to_your_corpus', 'r', encoding ='UTF8') as file:
+    corpus = file.readlines()
+    for sent in corpus:
+        doc = nlp(sent)
+        tokens = [token.text for token in doc]
+        print(tokens)
+
+    corpus = re.sub(r'\([^)]*\)', '', corpus) #remove information with brackets
+    corpus = re.sub(r'([\'\"\.\(\)\!\?\-\\\/\,])', '', corpus)
+    corpus = re.sub(r"[^a-z0-9]+",'', corpus.lower())
+    print("tokenizing")
+    corpus_tokenized = [nlp(sent) for sent in corpus]
+
 # Training # we get a bin file as a result
 print("training")
 model = fasttext.train_unsupervised(corpus_tokenized, model='skipgram', epoch=25, lr=0.05, minCount=2, dim=100)
